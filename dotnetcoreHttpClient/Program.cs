1990fc1324b437d6bb99ba839ceac456a204e492
盗理者 <952651117@qq.com>
2019-01-25 22:28:00 +0800

    f

---
+++ dotnetcoreHttpClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+
+namespace dotnetcoreHttpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient(new HttpClientHandler());
+            //获取我们当前网站的dom
+            var html = client.GetStringAsync("https://blog.csdn.net/qq_36051316/article/details/84872688").Result.ToString();
+            Console.WriteLine("输出网站内容：");
+            Console.WriteLine("================华丽分割线==================");
+            System.Console.WriteLine(html);
+        }
+    }
+}
